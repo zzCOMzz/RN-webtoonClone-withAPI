cf947a5dc1284eb92e5068f872caf51906c798e0
refnaldy <irvanrefnaldy98@gmail.com>
2019-10-10 12:31:18 +0700

    add share button fiture

--- src/components/buttonShare.js
+++ src/components/buttonShare.js
@@ -1,14 +1,21 @@
 import React, {Component} from 'react';
 import {Icon, Content} from 'native-base';
+import {Share} from 'react-native';
 
 class ButtonShare extends Component {
   constructor(props) {
     super(props);
   }
+  shareOptions = {
+    title: 'Webtoon Comic',
+    message: 'Read This Comic', // Note that according to the documentation at least one of "message" or "url" fields is required
+    url: 'www.example.com',
+    subject: 'Hello From Webtoon',
+  };
   render() {
     return (
       <Content style={{marginRight: 30}}>
-        <Icon name="share" />
+        <Icon onPress={() => Share.share(this.shareOptions)} name="share" />
       </Content>
     );
   }
